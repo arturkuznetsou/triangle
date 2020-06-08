b4c983bcb2e7e6a8bf52cfdcbb5532850cb4aa2e
Artur Kuznetsou <arturkuznetsou@pm.me>
2020-06-08 21:28:19 +0200

    Finished triangle generating functions

--- draw.go
+++ draw.go
@@ -10,15 +10,39 @@ func hline (x1 int, x2 int, y int) {
 			img.Set(n, y, color.Black)
 	}
 }
+func triangulate (x1 int, x2 int, y int) {
+	xma := x2 - x1 + 1
+	if xma > 9 {
+		xa := (x1 + x2) / 2
+		hline(x1 + xma / 4, x2 - xma / 4, y - xma / 2)
+		dline(x1 + xma / 4 - 1, y - xma / 2 + 1, -xma / 2)
+		dline(x2 - xma / 2 + 1, y, xma / 2)
+		triangulate(x1, x1 + (x2 - x1) / 2, y)
+		triangulate(xa + 1, x2, y)
+		triangulate(x1 + xma / 4, x2 - xma / 4, y - xma / 2)
+	}
+}
+func draw () {
+	hline(1, x * 8 - 1, x * 8)
+	dline(1, x * 8, x * 8)
+	dline(x * 4, 1, -x * 8)
+	triangulate(1, x * 8, x * 8)
+}
 /*
+ *
+ *
  * Function for drawing diagonal lines.
  * Starts at x, y and moves diagonaly
  * for each integer in height
  * Always goes to the right.
+ *
+ *
  */
+
 func dline (x int, y int, height int) {
 	add := 1
 	n := 0
+	height += height % 2
 	if height < 0 {
 		add = -1
 	}
--- main.go
+++ main.go
@@ -8,24 +8,16 @@ import (
 
 
 var img *i.RGBA
-func triangulate (y int, x1 int, x2 int) {
-	if math.Abs(float64(x2 - x1)) > 7 {
-	}
-}
-func setup () {
-}
+var x int
 func main () {
 	var depth float64
-	depth = 9
-	x := int(math.Pow(2, depth - 1))
+	depth = 10
+	x = int(math.Pow(2, depth - 1))
 	upLeft := i.Point{0, 0}
 	lowRight := i.Point{x * 8 + 1, x * 8 + 9}
 	img = i.NewRGBA(i.Rectangle{upLeft, lowRight})
 
-	hline(1, x * 8 - 1, x * 8)
-	dline(1, x * 8, x * 8)
-	dline(x * 4, 1, -x * 8)
-
+	draw()
 
 	f, _ := os.Create("image.png")
 	png.Encode(f, img)
